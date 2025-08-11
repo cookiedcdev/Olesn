eb13f6f5bd36ac367dc35bb33441cde7cc5f78b7
Cookie <165423332+cookiedcdev@users.noreply.github.com>
2025-08-12 00:02:45 +0100

    Update testing.lua

--- testing.lua
+++ testing.lua
@@ -39,8 +39,8 @@ TPC.OnClick:Connect(function()
 end)
 
 local KillAura = Mpage:AddToggle("Kill Aura", "Activates kill aura for enemies", false)
-local SliderDistance = Mpage:AddSlider("Distance For Kill Aura", "Distance for the kill aura to activate", 25, 25, 10000)
-local DistanceForKillAura = 25
+local SliderDistance = Mpage:AddSlider("Distance For Kill Aura", "Distance for the kill aura to activate (ONLY WORKS WHEN HOLDING YOUR WEAPON)", 200, 25, 10000)
+local DistanceForKillAura = 200
 SliderDistance.ValueUpdated:Connect(function(num)
     DistanceForKillAura = num
     print(num)
@@ -49,21 +49,17 @@ KillAura.OnToggle:Connect(function(value)
     KillA = value
     task.spawn(function()
         while KillA do
-            local player = game.Players.LocalPlayer
-            local character = player.Character or player.CharacterAdded:Wait()
+            local p = game.Players.LocalPlayer
+            local character = p.Character or p.CharacterAdded:Wait()
             local hrp = character:WaitForChild("HumanoidRootPart")
-            local weapon = (player.Inventory:FindFirstChild("Old Axe") or
-                                player.Inventory:FindFirstChild("Good Axe") or
-                                player.Inventory:FindFirstChild("Strong Axe") or
-                                player.Inventory:FindFirstChild("Chainsaw"))
+            local w = (p.Inventory:FindFirstChild("Old Axe") or p.Inventory:FindFirstChild("Good Axe") or p.Inventory:FindFirstChild("Strong Axe") 
+                or p.Inventory:FindFirstChild("Chainsaw"))
             task.spawn(function()
-                for _, bunny in pairs(workspace.Characters:GetChildren()) do
-                    if bunny:IsA("Model") and bunny.PrimaryPart then
-                        local distance = (bunny.PrimaryPart.Position - hrp.Position).Magnitude
+                for _, b in pairs(workspace.Characters:GetChildren()) do
+                    if b:IsA("Model") and b.PrimaryPart then
+                        local distance = (b.PrimaryPart.Position - hrp.Position).Magnitude
                         if distance <= DistanceForKillAura then
-                            local result =
-                                game:GetService("ReplicatedStorage").RemoteEvents.ToolDamageObject:InvokeServer(
-                                    bunny, weapon, 999, hrp.CFrame)
+                            local res = game:GetService("ReplicatedStorage").RemoteEvents.ToolDamageObject:InvokeServer(b, w, 999, hrp.CFrame)
                         end
                     end
                 end
@@ -73,8 +69,53 @@ KillAura.OnToggle:Connect(function(value)
     end)
     
 end)
-local page2 = lib:AddPage("Bring Item")
 
-local button = page2:AddButton("Reset walkspeed", "Resets walkspeed", "Reset")
+local TPS = Ppage:AddButton("Teleport To Stronghold", "Teleports you directly to the stronghold.")
+
+TPC.OnClick:Connect(function()
+    if Workspace.Map.Landmarks.Stronghold.Building.Exterior then
+        game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(Workspace.Map.Landmarks.Stronghold.Building.Exterior:GetChildren()[12].Model.Part.Position + Vector3.new(0, 15, 0))
+    end
+end)
+ 
+local TPDC = Ppage:AddButton("Teleport To Diamond Chest", "Teleports you directly to the diamond chest.")
+
+TPC.OnClick:Connect(function()
+    game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = Workspace.Map.Campground.MainFire.PrimaryPart.CFrame
+end)
+
+local function gethunger()
+    return math.floor(game:GetService("Players").LocalPlayer.PlayerGui.Interface.StatBars.HungerBar.Bar.Size.X.Scale * 100)
+end
+local chosen = "Carrot"
+local type = "Hunger %" 
+local food = {
+    "Apple",
+    "Berry",
+    "Carrot",
+    "Cake",
+    "Chili",
+    "Cooked Morsel",
+    "Cooked Steak"
+}
+local CF = AFpage:AddComboBox("Choose Food", "Choose a food to automatically eat.", food, chosen)
+CF.OnChanged:Connect(function(v) 
+    chosen = v
+end)
+
+local AEtype = AFpage:AddComboBox("Auto Eat Food Type", "Choose the type of automation.", {"Hunger %", "Every 3 Seconds"}, type)
+AEtype.OnChanged:Connect(function(v) 
+    type = v
+end)
+task.spawn(function()
+    local hn = AFpage:AddTextBox("Hunger:", gethunger())
+    local chosenfood = AFpage:AddTextBox("Chosen Food:", chosen)
+    while true do
+        task.wait(0.2)
+        hn.Description = gethunger()
+        chosenfood.Description = chosen
+    end
+end)
+
 
 lib:SetEnabled(true)
